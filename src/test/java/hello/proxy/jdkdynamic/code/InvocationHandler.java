2e66617b2e1cc5f9977b410f7e363194e2e609b4
장유진/전시개발팀 <yujin.jang@musinsa.com>
2021-12-22 13:35:21 +0900

    ch.5 동적 프로시 기술

---
+++ src/test/java/hello/proxy/jdkdynamic/code/InvocationHandler.java
@@ -0,0 +1,9 @@
+package hello.proxy.jdkdynamic.code;
+
+import java.lang.reflect.Method;
+
+public interface InvocationHandler {
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable;
+
+}
