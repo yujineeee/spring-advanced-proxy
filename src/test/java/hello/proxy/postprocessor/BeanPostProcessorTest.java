cdc47fddbc506073d1aa439a9cffbfa3f2298e69
장유진/전시개발팀 <yujin.jang@musinsa.com>
2022-01-12 22:31:32 +0900

    ch7. 빈 후처리

---
+++ src/test/java/hello/proxy/postprocessor/BeanPostProcessorTest.java
@@ -0,0 +1,73 @@
+package hello.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+public class BeanPostProcessorTest {
+
+    @Test
+    void basicConfig() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BasicConfig.class);
+
+        //A는 등록
+        B b = applicationContext.getBean("beanA", B.class);
+        b.helloB();
+
+        //B는 등록한 적 없음
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, ()-> applicationContext.getBean(A.class));
+    }
+
+    @Configuration
+    static class BasicConfig {
+
+        @Bean(name = "beanA")
+        public A a() {
+            return new A();
+        }
+
+        @Bean
+        public AToBPostProcessor helloPostProcessor(){
+            return new AToBPostProcessor();
+        }
+
+    }
+
+
+    static class A {
+        public void helloA() {
+            log.info("hello A");
+        }
+    }
+
+
+    static class B {
+        public void helloB() {
+            log.info("hello B");
+        }
+
+    }
+
+    static class AToBPostProcessor implements BeanPostProcessor {
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName)
+            throws BeansException {
+            log.info("beanName={} bean={}", beanName, bean);
+            if (bean instanceof A) {
+                return new B();
+            }
+
+            return bean;
+        }
+    }
+
+
+}
